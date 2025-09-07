b5ba646ffdcd79dcf21272e298b2567cd8dd5deb
Sonu <sonu@Sonu-MacBook-Air.local>
2025-09-07 19:54:31 +0530

    updated code and fix error in code

--- app.py
+++ app.py
@@ -769,4 +769,5 @@ def camera_control(command):
         return jsonify(status=f"Server error: {str(e)}"), 500
     
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
